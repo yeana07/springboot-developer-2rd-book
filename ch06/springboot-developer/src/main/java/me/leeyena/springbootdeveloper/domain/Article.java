6a2e8b8c92b57d76abfca5b5d4b142a350588879
yeana07 <lee.ye.naaa@gmail.com>
2024-09-06 20:47:55 +0900

    add ch06 code

---
+++ ch06/springboot-developer/src/main/java/me/leeyena/springbootdeveloper/domain/Article.java
@@ -0,0 +1,36 @@
+package me.leeyena.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder //빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
+
