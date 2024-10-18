ada793c46fda4f81804b199a29ac6e84c7ef8912
yeana07 <lee.ye.naaa@gmail.com>
2024-10-18 22:13:48 +0900

    add ch07 code

---
+++ ch07/springboot-developer/src/main/java/me/leeyena/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,51 @@
+package me.leeyena.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.leeyena.springbootdeveloper.domain.Article;
+import me.leeyena.springbootdeveloper.dto.ArticleListViewResponse;
+import me.leeyena.springbootdeveloper.dto.ArticleViewResponse;
+import me.leeyena.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles); //1 블로그 글 리스트 저장
+        
+        return "articleList"; // 2 articleList.html 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable(name = "id") long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    //1 id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(name = "id", required = false) Long id, Model model) {
+        if (id == null) { //2 id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else { //3 id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
+    }
+}
---
+++ ch07/springboot-developer/src/main/java/me/leeyena/springbootdeveloper/domain/Article.java
@@ -0,0 +1,50 @@
+package me.leeyena.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @CreatedDate //엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate //엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder //빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
+
